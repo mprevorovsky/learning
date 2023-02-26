88b06f6a7a2b1dce8e8255fc70bbd059577aec43
mprevorovsky <mprevorovsky@volny.cz>
2023-02-26 10:10:33 +0100

    Math - added overall score

--- math_practice/main.py
+++ math_practice/main.py
@@ -14,18 +14,24 @@ SYMBOL_OK = "💙"  # "🍭"
 class Window(pqw.QWidget):
     def __init__(self) -> None:
         super().__init__()
-        self.counter = 1
+        self.task_counter = 1
+        self.ok_counter = 0
+        self.wrong_counter = 0
         self.score = ""
         self.equation, self.result = self._generate_equation()
 
         self._setup()
         self.label_equation.setText(self.equation)
         self.label_score.setText(self.score)
+        self.label_overall_score.setText(
+            f"{SYMBOL_OK} {self.ok_counter}  {SYMBOL_WRONG} {self.wrong_counter}"
+        )
 
     def _setup(self) -> None:
         pqu.loadUi("math_practice.ui", self)
         self.label_equation = self.findChild(pqw.QLabel, "label_equation")
         self.label_score = self.findChild(pqw.QLabel, "label_score")
+        self.label_overall_score = self.findChild(pqw.QLabel, "label_overall_score")
         self.lineEdit_result = self.findChild(pqw.QLineEdit, "lineEdit_result")
         self.pushButton_again = self.findChild(pqw.QPushButton, "pushButton_again")
 
@@ -52,13 +58,15 @@ class Window(pqw.QWidget):
     def _evaluate_answer(self) -> None:
         if int(self.lineEdit_result.text()) == self.result:
             self.score = f"{self.score}{SYMBOL_OK}"
+            self.ok_counter += 1
         else:
             self.score = f"{self.score}{SYMBOL_WRONG}"
+            self.wrong_counter += 1
 
-        if self.counter < TASKS:
+        if self.task_counter < TASKS:
             self.equation, self.result = self._generate_equation()
             self.label_equation.setText(self.equation)
-            self.counter += 1
+            self.task_counter += 1
         else:
             self.label_equation.setText("Hotovo!")
             self.lineEdit_result.setEnabled(False)
@@ -67,9 +75,12 @@ class Window(pqw.QWidget):
 
         self.label_score.setText(self.score)
         self.lineEdit_result.setText("")
+        self.label_overall_score.setText(
+            f"{SYMBOL_OK} {self.ok_counter}  {SYMBOL_WRONG} {self.wrong_counter}"
+        )
 
     def _reset(self) -> None:
-        self.counter = 1
+        self.task_counter = 1
         self.score = ""
         self.equation, self.result = self._generate_equation()
         self.label_equation.setText(self.equation)
